7810502e4ccd56ef080e6c617dfe1ae547719970
leidypaez <leidy16paez@gmail.com>
2022-03-21 00:29:04 -0500

    subiendo el gravatar y diseño de pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { GravatarModule } from 'ngx-gravatar';
+import { RouterModule } from '@angular/router';
 
-@NgModule({
+@NgModule ({
   declarations: [
     AppComponent,
+
     LandingPageComponent,
     HomeComponent,
+
     FooterComponent
   ],
   imports: [
     BrowserModule,
+    GravatarModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
